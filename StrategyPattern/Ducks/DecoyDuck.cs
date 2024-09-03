9d4c3a9d61efa0311e5c56ccb9d07ec140e1ada9
Thijs Heerze <thijsheerze@gmail.com>
2024-09-03 18:42:01 +0200

    Strategy Pattern

---
+++ StrategyPattern/Ducks/DecoyDuck.cs
@@ -0,0 +1,23 @@
+﻿using StrategyPattern.Interfaces.FlyBehavior;
+using StrategyPattern.Interfaces.QuackBehavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern.Ducks
+{
+    internal class DecoyDuck : Duck
+    {
+        public DecoyDuck()
+        {
+            quackBehavior = new MuteQuack();
+            flyBehavior = new FlyNoWay();
+        }
+        public override void Display()
+        {
+            Console.WriteLine("\nI'm a Decoy Duck");
+        }
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using StrategyPattern.Ducks;
+using StrategyPattern.Interfaces.FlyBehavior;
+
+namespace StrategyPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new MallardDuck();
+            Duck redheadDuck = new RedheadDuck();
+            Duck decoyDuck = new DecoyDuck();
+            Duck rubberDuck = new RubberDuck();
+
+            MallardDuck mallard = new MallardDuck();
+            mallard.Display();
+            mallard.PerformQuack();
+            mallard.PerformFly();
+
+            RedheadDuck redhead = new RedheadDuck();
+            redhead.Display();
+            redhead.PerformQuack();
+            redhead.PerformFly();
+
+            DecoyDuck decoy = new DecoyDuck();
+            decoy.Display();
+            decoy.PerformQuack();
+            decoy.PerformFly();
+
+            RubberDuck rubber = new RubberDuck();
+            rubber.Display();
+            rubber.PerformQuack();
+            rubber.PerformFly();
+
+        }
+    }
+}
