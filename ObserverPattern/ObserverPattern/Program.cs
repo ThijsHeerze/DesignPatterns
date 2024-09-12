9e9f5df78c36203464f3bcded15dc0aafff3977b
Thijs Heerze <thijsheerze@gmail.com>
2024-09-12 15:52:32 +0200

    Observer Pattern done

--- ObserverPattern/ObserverPattern/Program.cs
+++ ObserverPattern/ObserverPattern/Program.cs
@@ -13,7 +13,7 @@ namespace ObserverPattern
             ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
             StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 
-            weatherData.SetMeasurements(28, 65, 30.4f);
+            weatherData.SetMeasurements(32, 70, 2.2f);
             weatherData.SetMeasurements(29, 70, 29.2f);
             weatherData.SetMeasurements(30, 90, 29.2f); 
 
